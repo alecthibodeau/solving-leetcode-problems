030e914604cd9a1eec04e7eb508a2dbd563e7ea5
alecthibodeau <alecthibodeau@gmail.com>
2022-03-08 06:54:52 -0500

    Solve Contains Duplicate problem

---
+++ contains-duplicate.js
@@ -0,0 +1,19 @@
+/*
+  217. Contains Duplicate
+  Run locally: $ node contains-duplicate.js
+ */
+
+const nums = [1, 2, 1, 3, 4];
+
+var containsDuplicate = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
+console.log(containsDuplicate(nums));
