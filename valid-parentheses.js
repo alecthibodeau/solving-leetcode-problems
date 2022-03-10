3329c012f3b6f2429a608869cfd4ad3cd73007f5
alecthibodeau <alecthibodeau@gmail.com>
2022-03-10 08:20:55 -0500

    Solve Valid Parentheses problem

---
+++ valid-parentheses.js
@@ -0,0 +1,18 @@
+/*
+  20. Valid Parentheses
+  Run locally: $ node valid-parentheses.js <string>
+ */
+
+  const s = process.argv[2];
+
+  var isValid = function(s) {
+      const originalLength = s.length;
+      let i = 0;
+      while (i < originalLength) {
+          s = s.replace(/\(\)/g, '').replace(/{}/g, '').replace(/\[\]/g, '');
+          i++;
+      }
+      return s ? false : true;
+  };
+
+console.log(isValid(s));
