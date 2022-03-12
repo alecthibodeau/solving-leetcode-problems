c15d061bdc775b543837c83479517ad06c079d39
alecthibodeau <alecthibodeau@gmail.com>
2022-03-12 09:34:17 -0500

    Fix Valid Parentheses indentation

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -3,16 +3,16 @@
   Run locally: $ node valid-parentheses.js <string>
  */
 
-  const s = process.argv[2];
+const s = process.argv[2];
 
-  var isValid = function(s) {
-      const originalLength = s.length;
-      let i = 0;
-      while (i < originalLength) {
-          s = s.replace(/\(\)/g, '').replace(/{}/g, '').replace(/\[\]/g, '');
-          i++;
-      }
-      return s ? false : true;
-  };
+var isValid = function(s) {
+    const originalLength = s.length;
+    let i = 0;
+    while (i < originalLength) {
+        s = s.replace(/\(\)/g, '').replace(/{}/g, '').replace(/\[\]/g, '');
+        i++;
+    }
+    return s ? false : true;
+};
 
 console.log(isValid(s));
