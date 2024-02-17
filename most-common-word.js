d575e17c93e2808d2c1b69f266de182b4a3ee627
alec t <alec@alect.me>
2024-02-17 09:24:46 -0500

    Solve Most Common Word problem

---
+++ most-common-word.js
@@ -0,0 +1,55 @@
+/**
+ * 819. Most Common Word
+ * Run locally: $ node most-common-word.js
+ */
+
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+
+// Testcase
+// const paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+// const banned = ["hit"];
+
+// Input example
+// const paragraph = "a, a, a, a, b,b,b,c, c";
+// const banned = ["a"];
+
+// Input example
+// const paragraph = "j. t? T. z! R, v, F' x! L; l! W. M; S. y? r! n; O. q; I? h; w. t; y; X? y, p. k! k, h, J, r? w! U! V; j' u; R! z. s. T' k. P? M' I' j! y. P, T! e; X. w? M! Y, X; G; d, X? S' F, K? V, r' v, v, D, w, K! S? Q! N. n. V. v. t? t' x! u. j; m; n! F, V' Y! h; c! V, v, X' X' t? n; N' r; x. W' P? W; p' q, S' X, J; R. x; z; z! G, U; m. P; o. P! Y; I, I' l' J? h; Q; s? U, q, x. J, T! o. z, N, L; u, w! u, S. Y! V; S? y' E! O; p' X, w. p' M, h! R; t? K? Y' z? T? w; u. q' R, q, T. R? I. R! t, X, s? u; z. u, Y, n' U; m; p? g' P? y' v, o? K? R. Q? I! c, X, x. r' u! m' y. t. W; x! K? B. v; m, k; k' x; Z! U! p. U? Q, t, u' E' n? S' w. y; W, x? r. p! Y? q, Y. t, Z' V, S. q; W. Z, z? x! k, I. n; x? z; V? s! g, U; E' m! Z? y' x? V! t, F. Z? Y' S! z, Y' T? x? v? o! l; d; G' L. L, Z? q. w' r? U! E, H. C, Q! O? w! s? w' D. R, Y? u. w, N. Z? h. M? o, B, g, Z! t! l, W? z, o? z, q! O? u, N; o' o? V; S! z; q! q. o, t! q! w! Z? Z? w, F? O' N' U' p? r' J' L; S. M; g' V. i, P, v, v, f; W? L, y! i' z; L? w. v, s! P?";
+// const banned = ["m","q","e","l","c","i","z","j","g","t","w","v","h","p","d","b","a","r","x","n"];
+
+// Input example
+const paragraph = "..Bob hit a ball, the hit BALL flew far after it was hit.";
+const banned = ["hit"];
+
+var mostCommonWord = function(paragraph, banned) {
+  const spaceAndSymbolsRegex = /[ !?',;.]+/;
+  const wordsNotBanned = paragraph.toLowerCase().split(spaceAndSymbolsRegex).filter(word => !banned.includes(word) && word !== '');
+  const wordsFrequency = {};
+  let mostFrequentWord;
+  let maximumFrequency = 0;
+
+  wordsNotBanned.forEach(word => {
+    wordsFrequency[word] = (wordsFrequency[word] || 0) + 1;
+  });
+
+  for (const word in wordsFrequency) {
+    if (wordsFrequency[word] > maximumFrequency) {
+      mostFrequentWord = word;
+      maximumFrequency = wordsFrequency[word];
+    }
+  }
+
+  return mostFrequentWord;
+};
+
+console.log(mostCommonWord(paragraph, banned));
+
+/**
+ * Accepted
+ * Runtime: 57ms Beats 75.60% of users with JavaScript
+ * Memory: 50.44 MB Beats 51.85% of users with JavaScript
+ */
