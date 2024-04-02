171756cf51c69b5226b839984f7292084a8a7e7a
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2024-04-01 20:25:54 -0400

    Solve Add Two Integers problem

---
+++ add-two-integers.js
@@ -0,0 +1,24 @@
+/**
+ * 2235. Add Two Integers
+ * Run locally: $ node add-two-integers.js
+ */
+
+// Input example 1
+// const num1 = 12;
+// const num2 = 5;
+
+// Input example 2
+const num1 = -10;
+const num2 = 4;
+
+var sum = function(num1, num2) {
+  return num1 + num2;
+};
+
+console.log(sum(num1, num2));
+
+/**
+ * Accepted
+ * Runtime: 50 ms, Beats 71.03% of users with JavaScript
+ * Memory: 48.78 MB, Beats 60.89% of users with JavaScript
+ */
